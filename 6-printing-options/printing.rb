485581e884730e8033fe11325a8bd24bbf4c5ff1
ridaffa <47104358+ridaffa@users.noreply.github.com>
2023-03-04 23:44:43 +0700

    feat: printing options

---
+++ 6-printing-options/printing.rb
@@ -0,0 +1,7 @@
+puts "A string"
+# puts mean "print" in Ruby
+# puts iterate the value of array and print each value
+
+p "A string"
+# p mean "print" and also return the value
+# p print entire array
