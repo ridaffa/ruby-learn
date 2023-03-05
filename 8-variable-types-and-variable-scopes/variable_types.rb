3de8dd909d03dad53b58ce19dbb38fab012c6dfd
ridaffa <47104358+ridaffa@users.noreply.github.com>
2023-03-05 16:23:54 +0700

    feat: variable types

---
+++ 8-variable-types-and-variable-scopes/variable_types.rb
@@ -0,0 +1,26 @@
+#Local Variables
+
+10.times do
+  x = 10
+end
+
+#Global Variables
+#Dont do it 
+
+10.times do
+  $x = 10
+end
+
+# Instance Variables
+@batting_average = 300
+
+#Constant Variables
+TEAM = "Angels"
+
+# Class Variables
+class MyClass
+  @@teams = ["A's", "Tigers"]
+end
+
+
+
