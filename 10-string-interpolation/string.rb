9ba261efbdb0916411411876b56ddab416be249a
ridaffa <47104358+ridaffa@users.noreply.github.com>
2023-03-05 17:28:05 +0700

    feat: string

---
+++ 10-string-interpolation/string.rb
@@ -0,0 +1,9 @@
+puts "Name an animal"
+animal = gets.chomp
+
+puts "Name a noun"
+noun = gets.chomp
+
+p "The quick brown #{animal} jumped over the lazy #{noun}."
+
+p "2 + 2 = #{2 + 2}"
---
+++ 11-string-manipulation/string.rb
@@ -0,0 +1,14 @@
+"Astros".swapcase
+"Astros".upcase
+"Astros".reverse
+"Astros".reverse.upcase
+
+m = "Milk"
+m.upcase!
+p m
+#! is a bang method, it changes the original value
+#without ! it will return a new value
+
+p m
+
+# ruby-doc.rg/core-2.2.0/String.html
